526ac4c74bc91f363b2150ee27a399ebd7c705e1
ger <ger>
2001-10-11 10:44:39 +0000

    fubar

--- sourceagency/tests/constants.php
+++ sourceagency/tests/constants.php
@@ -4,13 +4,13 @@
 // Author: Gerrit Riessen, gerrit.riessen@open-source-consultants.de
 // Copyright (C) 2001 Gerrit Riessen
 // 
-// $Id: constants.php,v 1.4 2001/10/11 10:03:39 ger Exp $
+// $Id: constants.php,v 1.5 2001/10/11 10:44:39 ger Exp $
 //
 // php library root directory
 $LIB_ROOT = "/www/development/lib";
 
 ini_set('include_path', ini_get('include_path') . ':'.$LIB_ROOT.'/php' );
-// php unit test framework
+//  php unit test framework
 require_once("phpunit.php");
 
 function define_test_suite( $filename ) {
