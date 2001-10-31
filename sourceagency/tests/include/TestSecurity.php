375c8898f9415ca9f4daed64850343264ff1e922
riessen <riessen>
2001-10-31 15:45:05 +0000

    more tests

--- sourceagency/tests/include/TestSecurity.php
+++ sourceagency/tests/include/TestSecurity.php
@@ -4,7 +4,7 @@
 // Author: Gerrit Riessen, gerrit.riessen@open-source-consultants.de
 // Copyright (C) 2001 Gerrit Riessen
 // 
-// $Id: TestSecurity.php,v 1.2 2001/10/31 12:23:16 riessen Exp $
+// $Id: TestSecurity.php,v 1.3 2001/10/31 15:45:05 riessen Exp $
 
 include_once( "../constants.php" );
 
@@ -26,6 +26,7 @@ extends TestCase
 
     function testIs_sponsor() {
         global $auth;
+        $auth = new Auth;
 
         $user1 = "fubar";
         $user2 = "snafu";
@@ -80,12 +81,13 @@ extends TestCase
 
     function testIs_accepted_sponsor() {
         global $auth;
+        $auth = new Auth;
 
         $user1 = "fubar"; $proid1 = "proid1";
         $user2 = "snafu"; $proid2 = "proid2";
 
         $db_config = new mock_db_configure;
-        $db_config->set_nr_instance_expected( 2 );
+        $db_config->set_nr_instance_expected( 4 );
         $db_q = array( // Arg: 1=proid, 2=sponsor name
                        0 => ("SELECT * FROM sponsoring WHERE proid='%s'"
                              . " AND status='A' AND sponsor='%s'"));
@@ -108,7 +110,18 @@ extends TestCase
         $auth->set_perm( "Hell Yes!" );
         $this->assertEquals( 1, is_accepted_sponsor( $proid2 ) );
 
-        // TODO: complete test: test unset cases: auth and 'perm'
+        //
+        // perm is not set
+        //
+        $auth->set_uname( "" );
+        $auth->set_perm( "" );
+        $this->assertEquals( 0, is_accepted_sponsor( "" ) );
+        
+        //
+        // unset the auth
+        //
+        unset( $auth );
+        $this->assertEquals( 0, is_accepted_sponsor( "" ) );
 
         // if using a database, then ensure that it didn't fail
         $this->assertEquals(false, $db_config->did_db_fail(),
@@ -117,25 +130,49 @@ extends TestCase
 
     function testIs_accepted_referee() {
         global $auth;
+        $auth = new Auth;
 
         $user1 = "fubar"; $proid1 = "proid1";
         $user2 = "snafu"; $proid2 = "proid2";
 
-        //
-        // TODO: do this test! (it was number 50!)
-        //
-
         $db_config = new mock_db_configure;
-        $db_config->set_nr_instance_expected( 0 );
+        $db_config->set_nr_instance_expected( 4 );
         $db_q = array( // Arg: 1=proid, 2=sponsor name
                        0 => ("SELECT * FROM referees WHERE proid='%s' AND"
                              . " status='A' AND referee='%s'"));
 
         $db_config->add_query( sprintf( $db_q[0], $proid1, $user1  ), 0 ); 
         $db_config->add_query( sprintf( $db_q[0], $proid2, $user2  ), 1 ); 
-        $db_config->add_num_row( 0, 0 );
-        $db_config->add_num_row( 1, 1 );
+        $db_config->add_num_row( 1, 0 );
+        $db_config->add_num_row( 0, 1 );
 
+        //
+        // user1 query
+        //
+        $auth->set_uname( $user1 );
+        $auth->set_perm( "Hell Yes!" );
+        $this->assertEquals( 1, is_accepted_referee( $proid1 ), __LINE__ );
+
+        //
+        // user2 query
+        //
+        $auth->set_uname( $user2 );
+        $auth->set_perm( "Hell Yes!" );
+        $this->assertEquals( 0, is_accepted_referee( $proid2 ), __LINE__ );
+
+        //
+        // perm is empty
+        //
+        $auth->set_uname( "yet another user" );
+        $auth->set_perm( "" );
+        $this->assertEquals( 0, is_accepted_referee( "proid" ), __LINE__ );
+
+        //
+        // auth is unset
+        //
+        unset( $auth );
+        $this->assertEquals( 0, is_accepted_referee( "proid" ), __LINE__ );
+        
         // if using a database, then ensure that it didn't fail
         $this->assertEquals(false, $db_config->did_db_fail(),
                             $db_config->error_message() );
