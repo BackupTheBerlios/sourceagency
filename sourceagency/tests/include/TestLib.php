9efac69c9b29047917c05240d3d16555cc097879
riessen <riessen>
2002-01-09 16:24:57 +0000

    added two more test methods ....

--- sourceagency/tests/include/TestLib.php
+++ sourceagency/tests/include/TestLib.php
@@ -16,7 +16,7 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 or later of the GPL.
 #
-# $Id: TestLib.php,v 1.7 2001/12/13 16:52:29 riessen Exp $
+# $Id: TestLib.php,v 1.8 2002/01/09 16:24:57 riessen Exp $
 #
 ######################################################################
 
@@ -30,15 +30,25 @@ if ( !defined("BEING_INCLUDED" ) ) {
     // global translation object
     include_once( "translation.inc" );
     $t = new translation("English");
+
+    include_once( "box.inc" );
+    $bx = new box;
 }
 
 include_once( 'lib.inc' );
 
 class UnitTestLib
-extends TestCase
+extends UnitTest
 {
     function UnitTestLib( $name ) {
         $this->TestCase( $name );
+    }
+
+    function setup() {
+        // Called before each test method.
+        // if using the capturing routines then ensure that it's reset,
+        // it uses global variables
+        capture_reset_text();
     }
 
     function testMonth() {
@@ -149,6 +159,7 @@ extends TestCase
     }
 
     function testTimestr() {
+        setlocale( LC_TIME, "de_DE" );
         $this->assertEquals( "Montag, 15. Oktober 2001, 19:09:48 CEST",
                              timestr( 1003165788 ) );
         $this->assertEquals( "Montag, 15. Oktober 2001, 19:09:58 CEST",
@@ -216,6 +227,175 @@ extends TestCase
         
         $this->assertEquals( "a\nbc\n12345\nabc",
                              wrap( "a bc 12345 abc", 2, " " ));
+    }
+
+    function testCalendar_box() {
+        $db_config = new mock_db_configure;
+        // 4 instances required: 2 for the no budget case, and 2 for the
+        // budget is set case.
+        $db_config->set_nr_instance_expected( 4 );
+        $db_q = array( 0 => ("SELECT * FROM description,auth_user WHERE "
+                             . "proid='%s' AND description_user = username"),
+                       1 => ("SELECT SUM(budget) FROM sponsoring WHERE "
+                             . "proid='%s' AND status='A'"));
+        $dat = array( "r0" => "proid1", "e0" => 1,
+                      "r1" => "proid1", "e1" => 0,
+                      "r2" => "proid2", "e2" => 1,
+                      "r3" => "proid2", "e3" => 1 );
+
+        $db_config->add_query( sprintf( $db_q[0], $dat["r0"] ), 0 );
+        $db_config->add_query( sprintf( $db_q[1], $dat["r1"] ), 1 );
+        $db_config->add_query( sprintf( $db_q[0], $dat["r2"] ), 2 );
+        $db_config->add_query( sprintf( $db_q[1], $dat["r3"] ), 3 );
+        $db_config->add_num_row( $dat["e0"], 0 );
+        $db_config->add_num_row( $dat["e1"], 1 );
+        $db_config->add_num_row( $dat["e2"], 2 );
+        $db_config->add_num_row( $dat["e3"], 3 );
+
+        $row1 = $this->_generate_array(array("description_user", "volume",
+                                            "type","description_creation"),1);
+        $db_config->add_record( $row1, 0 );
+        $row3 = $this->_generate_array(array("description_user", "volume",
+                                            "type","description_creation"),2);
+        $db_config->add_record( $row3, 2 );
+        $row2 = array( "SUM(budget)" => "INCORRECT" );
+        $db_config->add_record( $row2, 1 );
+        $row4 = array( "SUM(budget)" => 1000 );
+        $db_config->add_record( $row4, 3 );
+
+        //
+        // no budget -- this should actually do something else, i.e.
+        // display a zero budget amount, however the code is broken and
+        // has a fixme note
+        //
+        capture_start();
+        calendar_box( $dat["r0"] );
+        capture_stop();
+
+        $text = capture_text_get();
+        $this->_testFor_length( 2174 );
+        $this->_testFor_pattern( $text, ("<b>Project Owner\(s\):<\/b><\/td>\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         . "<td align=\"left\" width=\"45%\""
+                                         . " bgcolor=\"#FFFFFF\">&nbsp;"
+                                         .$row1["description_user"]."<\/td>"));
+        $this->_testFor_pattern( $text, ("<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\"><b>Project "
+                                         ."Type:<\/b><\/td>\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         ."<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\">&nbsp;"
+                                         .$row1["type"]."<\/td>"));
+        $this->_testFor_pattern( $text, ("<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\"><b>Project "
+                                         ."Volume:<\/b><\/td>\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         ."<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\">&nbsp;"
+                                         .$row1["volume"]
+                                         ."<\/td>"));
+        // TODO: this is wrong, 100 euro should not appear here ....
+        $this->_testFor_pattern( $text, ("<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\"><b>Current "
+                                         ."project budget:<\/b><\/td>\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         ."<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\">INCORRECT euro"
+                                         ."<\/td>"));
+
+        //
+        // This run has a budget and the budget value should be printed
+        capture_reset_text();
+        capture_start();
+        calendar_box( $dat["r2"] );
+        capture_stop();
+
+        $text = capture_text_get();
+        $this->_testFor_length( 2169 );
+        $this->_testFor_pattern( $text, ("<b>Project Owner\(s\):<\/b><\/td>\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         . "<td align=\"left\" width=\"45%\""
+                                         . " bgcolor=\"#FFFFFF\">&nbsp;"
+                                         .$row3["description_user"]."<\/td>"));
+        $this->_testFor_pattern( $text, ("<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\"><b>Project "
+                                         ."Type:<\/b><\/td>\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         . $this->p_regexp_html_comment . "\n"
+                                         ."<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\">&nbsp;"
+                                         .$row3["type"]."<\/td>"));
+        $this->_testFor_pattern( $text, ("<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\"><b>Project "
+                                         ."Volume:<\/b><\/td>\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         ."<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\">&nbsp;"
+                                         .$row3["volume"]
+                                         ."<\/td>"));
+        // TODO: this is wrong, 100 euro should not appear here ....
+        $this->_testFor_pattern( $text, ("<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\"><b>Current "
+                                         ."project budget:<\/b><\/td>\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         .$this->p_regexp_html_comment . "\n"
+                                         ."<td align=\"left\" width=\"\" "
+                                         ."bgcolor=\"#FFFFFF\">"
+                                         .$row4["SUM(budget)"]
+                                         ." euro<\/td>"));
+
+        // check that the database component did not fail
+        $this->assertEquals( false, $db_config->did_db_fail(),
+                             $db_config->error_message() );
+    }
+
+    function testCheckcnt() {
+        // need to instantiate a new DB_SourceAgency object for the
+        // global $db variable
+        global $db;
+
+        $db_config = new mock_db_configure;
+        $db_config->set_nr_instance_expected( 2 );
+        $db_q = array( 0 => ("DELETE FROM counter_check WHERE "
+                             ."DATE_FORMAT(creation_cnt,'%Y-%m-%d') != '"
+                             . date("Y-m-d") . "'"),
+                       1 => ("SELECT * FROM counter_check WHERE "
+                             ."proid='%s' AND cnt_type='%s' AND ipaddr='%s'"),
+                       2 => ("INSERT counter_check SET proid='%s',"
+                             ."cnt_type='%s',ipaddr='%s'"));
+        $dat = array( "p1" => "proid1", "t1" => "type1", "ip1" => "ipaddr1",
+                      "p2" => "proid2", "t2" => "type2", "ip2" => "ipaddr2");
+
+        $db_config->add_query( $db_q[0], 0 );
+        $db_config->add_query( sprintf( $db_q[1], $dat["p1"], $dat["t1"],
+                                        $dat["ip1"] ), 0 );
+
+        $db_config->add_query( sprintf( $db_q[2], $dat["p1"], $dat["t1"],
+                                        $dat["ip1"] ), 0 );
+
+        $db_config->add_query( $db_q[0], 1 );
+        $db_config->add_query( sprintf( $db_q[1], $dat["p2"], $dat["t2"],
+                                        $dat["ip2"] ), 1 );
+
+        $db_config->add_num_row( 0, 0 );
+        $db_config->add_num_row( 1, 1 );
+
+        // redefine the global variable $db with the configured database
+        $db = new DB_SourceAgency;
+        $this->assertEquals( 1, checkcnt( $dat["p1"], $dat["ip1"],$dat["t1"]));
+
+        $db = new DB_SourceAgency;
+        $this->assertEquals( 0, checkcnt( $dat["p2"], $dat["ip2"],$dat["t2"]));
+
+        // check that the database component did not fail
+        $this->assertEquals( false, $db_config->did_db_fail(),
+                             $db_config->error_message() );
     }
 }
 
