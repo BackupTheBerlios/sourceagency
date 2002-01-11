f24cb689d9fcd7b8b5d39e35554ec30db1cc17d7
riessen <riessen>
2002-01-11 13:37:00 +0000

    refactored and extended to allow the turning off the checking of specific errors and also added a function for checking whether did_db_fail was called for an instance of the mock_db_configure class

--- sourceagency/tests/mock_database.php
+++ sourceagency/tests/mock_database.php
@@ -15,7 +15,7 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 or later of the GPL.
 #
-# $Id: mock_database.php,v 1.12 2002/01/09 16:14:17 riessen Exp $
+# $Id: mock_database.php,v 1.13 2002/01/11 13:37:00 riessen Exp $
 #
 ######################################################################
 
@@ -33,6 +33,23 @@ include_once( 'phpunit.php' );
 // globals required by our database class. These should not be set
 // directly, only referenced through the configuration class.
 
+// these are the flags that can be used in the ignore error method of the
+// configuration class
+define( "MKDB_NO_ERRORS", 0x00 );
+
+define( "MKDB_QUERY_COUNT", 0x01 ); // whether all queries were used
+define( "MKDB_RECORD_COUNT", 0x02 ); // whether all data records were used
+define( "MKDB_QUERY_EXISTS", 0x04 ); // is query available for query(..) call
+define( "MKDB_QUERY_COMPARE", 0x08 ); // compare queries (expected/actual)
+define( "MKDB_FIELD_SET", 0x10 ); // field in record is set
+define( "MKDB_NUM_ROWS", 0x20 ); // number of rows array size check (num_rows)
+define( "MKDB_NEXT_RECORD", 0x40 ); // enough data records avail. (next_record)
+
+// if adding more errors, then don't forget to update MKDB_ALL_ERRORS
+define( "MKDB_ALL_ERRORS", MKDB_QUERY_COUNT | MKDB_RECORD_COUNT 
+        | MKDB_QUERY_EXISTS | MKDB_QUERY_COMPARE | MKDB_FIELD_SET 
+        | MKDB_NUM_ROWS | MKDB_NEXT_RECORD );
+
 // *****************
 // the following data structures are all arrays of arrays. The first
 // array is indexed by the instance number,
@@ -41,7 +58,8 @@ include_once( 'phpunit.php' );
 // should be reset 
 $g_mkdb_queries = array(); 
 // array of values that should returned for successive calls to the
-// num_rows method of the mock_database class
+// num_rows method of the mock_database class (successive calls because
+// next_record can reduce the value that num_rows should return).
 $g_mkdb_num_rows = array();
 // an array of arrays for storing the values for a row of values
 // The second arrays should be indexed with column names.
@@ -55,6 +73,10 @@ $g_mkdb_cur_record = array();
 // Here ends variables that are arrays of arrays ....
 // ************************
 
+// this is an array indexed by instance number and containing 
+// flags to tell which errors should be ignored for the instance
+$g_mkdb_ignore_error = array();
+
 // this is set to true if the database failed for some reason or other
 $g_mkdb_failed = false;
 $g_mkdb_failure_text = "";
@@ -63,19 +85,44 @@ $g_mkdb_failure_text = "";
 $g_mkdb_instance_counter = 0;
 $g_mkdb_nr_instance_expected = 1;
 
+$g_mkdb_config_instance_count = 0;
+$g_mkdb_config_did_db_fail_called = array();
+
 // every time a mock database is required, an new object of this
 // class should be created and used to configure the mock database
 // that will be used.
 class mock_db_configure
 {
-    function mock_db_configure() {
+    var $instance_num = -1;
+
+    // instance_count is the number of instance we are going to need
+    function mock_db_configure( $instance_count = 1 ) {
         global $g_mkdb_failed, $g_mkdb_failure_text, $g_mkdb_instance_counter;
+
+        // this is used to ensure that each instance of the database 
+        // configuration class has the did_db_fail method called, if not,
+        // then this prints a warning....
+        $this->_increment_instance_counter();
 
         $g_mkdb_failed = false;
         $g_mkdb_failure_text = ">>>>Database failed<<<<<br>\n";
 
         $g_mkdb_instance_counter = 0;
-        $this->set_nr_instance_expected( 1 );
+        $this->set_nr_instance_expected( $instance_count );
+    }
+
+    function _did_db_fail_called() {
+        global $g_mkdb_config_did_db_fail_called;
+        $g_mkdb_config_did_db_fail_called[$this->instance_num] = true;
+    }
+
+    function _increment_instance_counter() {
+        global $g_mkdb_config_instance_count,$g_mkdb_config_did_db_fail_called;
+
+        $g_mkdb_config_did_db_fail_called[$g_mkdb_config_instance_count]=false;
+
+        $this->instance_num = $g_mkdb_config_instance_count;
+        $g_mkdb_config_instance_count++;
     }
 
     // this should be called fairly early in the configuration phase,
@@ -84,8 +131,10 @@ class mock_db_configure
     function set_nr_instance_expected( $instance_count ) {
         global $g_mkdb_nr_instance_expected, $g_mkdb_cur_num_row_call,
             $g_mkdb_cur_query_call, $g_mkdb_cur_record, $g_mkdb_queries,
-            $g_mkdb_num_rows, $g_mkdb_next_record_data;
+            $g_mkdb_num_rows, $g_mkdb_next_record_data, $g_mkdb_ignore_error,
+            $g_mkdb_instance_counter;
 
+        $g_mkdb_instance_counter = 0;
         $g_mkdb_nr_instance_expected = $instance_count;
 
         $g_mkdb_cur_num_row_call = array();
@@ -96,10 +145,14 @@ class mock_db_configure
         $g_mkdb_num_rows = array();
         $g_mkdb_next_record_data = array();
 
+        $g_mkdb_ignore_errors = array();
+
         for ( $idx = 0; $idx < $g_mkdb_nr_instance_expected; $idx++ ) {
             $g_mkdb_cur_num_row_call[$idx] = 0;
             $g_mkdb_cur_query_call[$idx] = 0;
             $g_mkdb_cur_record[$idx] = 0;
+
+            $g_mkdb_ignore_error[$idx] = MKDB_NO_ERRORS;
 
             $g_mkdb_queries[$idx] = array(); 
             $g_mkdb_num_rows[$idx] = array();
@@ -130,28 +183,41 @@ class mock_db_configure
                                 $record, $index );
     }
 
+    // method can be used to turn off specific error checking for specific
+    // instances of the database.
+    function ignore_errors( $error = MKDB_NO_ERRORS, $inst_nr = 0 ) {
+        global $g_mkdb_ignore_error;
+        $g_mkdb_ignore_error[ $inst_nr ] = $error;
+    }
+
     // returns true if the database failed. This also checks whether 
     // the number of expected instance equals the number of instances
     // actually created .....
     function did_db_fail() {
         global $g_mkdb_failed, $g_mkdb_failure_text, $g_mkdb_instance_counter,
             $g_mkdb_nr_instance_expected, $g_mkdb_cur_query_call,
-            $g_mkdb_queries;
+            $g_mkdb_queries, $g_mkdb_next_record_data, $g_mkdb_cur_record,
+            $g_mkdb_ignore_error;
+
+        $this->_did_db_fail_called();
 
         if ( $g_mkdb_instance_counter != $g_mkdb_nr_instance_expected ) {
             $g_mkdb_failure_text 
-                 .= ("<br>\n<br>\n****** Instance creation count mismatch "
-                     ."******<br>\nExpected: " . $g_mkdb_nr_instance_expected 
+                 .= ("<br>\n<br>\n*** Instance creation count mismatch "
+                     ."***<br>\nExpected: " . $g_mkdb_nr_instance_expected 
                      . " but created: " . $g_mkdb_instance_counter . "<br>\n");
             $g_mkdb_failed = true;
         } else {
             // check whether every instance also used all of it's queries,
             // this detects whether more queries were defined than used.
+            // this check can be turned off using MKDB_QUERY_COUNT
             for ( $idx = 0; $idx < $g_mkdb_instance_counter; $idx++ ) {
                 $query_count_actual = $g_mkdb_cur_query_call[$idx];
                 $query_count_expected = count( $g_mkdb_queries[$idx] );
 
-                if ( $query_count_expected != $query_count_actual ) {
+                if ( $query_count_expected != $query_count_actual 
+                    && (($g_mkdb_ignore_error[$idx] & MKDB_QUERY_COUNT) 
+                        != MKDB_QUERY_COUNT)) {
                     $g_mkdb_failure_text
                          .= ("<br>\nInstance " . $idx . " did not use"
                              ." all of its defined queries: expected = "
@@ -160,8 +226,26 @@ class mock_db_configure
                     $g_mkdb_failed = true;
                 }
             }
-        }
 
+            // check whether all rows for the next_record method were used up
+            // this check can be turned off using the MKDB_RECORD_COUNT
+            for ( $idx = 0; $idx < $g_mkdb_instance_counter; $idx++ ) {
+                $next_record_data = $g_mkdb_next_record_data[$idx];
+                $cur_record = $g_mkdb_cur_record[$idx];
+                
+                if ( $cur_record < count( $next_record_data )
+                     && (($g_mkdb_ignore_error[$idx] & MKDB_RECORD_COUNT) 
+                         != MKDB_RECORD_COUNT)) {
+                    $g_mkdb_failure_text
+                         .= ("<br>\nInstance " . $idx . " did not use"
+                             ." all of its defined records: available = "
+                             . count( $next_record_data ) . ", used = "
+                             . $cur_record . "<br>\n" );
+                    $g_mkdb_failed = true;
+                }
+            }
+        }
+        
         return $g_mkdb_failed;
     }
 
@@ -194,17 +278,29 @@ extends Assert
 {
     var $cur_fetch_row = array();
     var $instance_number = -1;
+    // flag which says which errors should be ignored
+    var $ignore_error_flag = MKDB_NO_ERRORS;
 
     function mock_database() {
-        global $g_mkdb_instance_counter, $g_mkdb_nr_instance_expected;
+        global $g_mkdb_instance_counter, $g_mkdb_nr_instance_expected,
+            $g_mkdb_ignore_error;
         // TODO: should be semaphored when accessing instance counter
         $this->instance_number = $g_mkdb_instance_counter++;
-        if ( $this->instance_number > $g_mkdb_nr_instance_expected ) {
-            // if there are more instances than expected, throw exception
-            $this->assertEquals( $this->instance_number,
-                                 $g_mkdb_nr_instance_expected,
-                                 "too many mock_database instance created" );
-        }
+
+        // assert that we're creating too many instances
+        $this->assert( $this->instance_number <= $g_mkdb_nr_instance_expected,
+                       "too many instance created, expected: "
+                       . $g_mkdb_nr_instance_expected . ", have: "
+                       . $this->instance_number);
+
+        $this->ignore_error_flag = 
+             $g_mkdb_ignore_error[ $this->instance_number ];
+    }
+
+    // private function for query the $ignore_error_flag value, returns
+    // true if we should be checking for a particular error 
+    function _check_for( $error ) {
+        return ( ( $this->ignore_error_flag & $error) != $error );
     }
 
     function query( $query_string ) {
@@ -214,15 +310,20 @@ extends Assert
         $queries = $g_mkdb_queries[$this->instance_number];
 
         // check whether we have enough queries to satisy the query call
-        $this->assert( $cur_query_call < count($queries),
-                       "mock_database(query): no query for call = " 
-                       . $cur_query_call . ", query: [" . $query_string . "]");
+        if ( $this->_check_for(MKDB_QUERY_EXISTS)) {
+            $this->assert( $cur_query_call < count($queries),
+                           "mock_database(query): no query for call = " 
+                           . $cur_query_call . ", query: [" 
+                           . $query_string . "]");
+        }
 
         // check that the query string passed as argument and the one
         // expected match up.
-        $this->assertEquals( $queries[$cur_query_call],
-                             $query_string, "mock_database(query): query "
-                             . "mismatch, call = " . $cur_query_call );
+        if ( $this->_check_for( MKDB_QUERY_COMPARE ) ) {
+            $this->assertEquals( $queries[$cur_query_call],
+                                 $query_string, "mock_database(query): query "
+                                 . "mismatch, call = " . $cur_query_call );
+        }
 
         $g_mkdb_cur_query_call[$this->instance_number]++;
     }
@@ -232,9 +333,13 @@ extends Assert
 
         $cur_record = $g_mkdb_cur_record[$this->instance_number];
 
-        $this->assertEquals( true, isset( $this->cur_fetch_row[$column_name] ),
-                             "mock_database(fetch): '$column_name' was not "
-                             . "set, current row: " . $cur_record );
+        // assert that the field is set, i.e. defined, in the record
+        if ( $this->_check_for( MKDB_FIELD_SET ) ) {
+            $this->assert( isset( $this->cur_fetch_row[$column_name] ),
+                           "mock_database(fetch): '$column_name' was not "
+                           . "set, current row: " . $cur_record );
+        }
+
         return $this->cur_fetch_row[ $column_name ];
     }
 
@@ -248,13 +353,19 @@ extends Assert
     function num_rows() {
         global $g_mkdb_num_rows, $g_mkdb_cur_num_row_call;
 
+        // g_mkdb_num_rows contains arrays because each call to num_rows
+        // may have been affected by a next_record call, i.e. while(num_rows())
+        // { next_record(); } ... although normally while(next_record()) {...}
+        // is used, the first variation can not be excluded
         $num_rows = $g_mkdb_num_rows[$this->instance_number];
         $cur_num_row_call = $g_mkdb_cur_num_row_call[$this->instance_number];
 
-        $this->assertEquals(false, $cur_num_row_call 
-                            >= count($num_rows),
-                            "mock_database(num_rows): no more rows available,"
-                            . " call = " . $cur_num_row_call);
+        if ( $this->_check_for( MKDB_NUM_ROWS ) ) {
+            // check that we have enough data
+            $this->assert($cur_num_row_call < count($num_rows),
+                          "mock_database(num_rows): no more rows available,"
+                          . " call = " . $cur_num_row_call);
+        }
 
         $g_mkdb_cur_num_row_call[$this->instance_number]++;
         return $num_rows[ $cur_num_row_call ];
@@ -268,12 +379,22 @@ extends Assert
         $next_record_data = $g_mkdb_next_record_data[$this->instance_number];
         $cur_record = $g_mkdb_cur_record[$this->instance_number];
 
-        $this->assertEquals(false, $cur_record >= count($next_record_data),
-                            "mock_database(next_record): no more data,"
-                            . " record = " . $cur_record);
+        // cause an error if next_record is called _again_ after it already
+        // has returned false, hence the +1 on the count($next_record_data)
+        if ( $this->_check_for( MKDB_NEXT_RECORD ) ) {
+            $this->assert($cur_record < count($next_record_data) + 1,
+                          "mock_database(next_record): no more data,"
+                          . " record = " . $cur_record);
+        }
         
         $g_mkdb_cur_record[$this->instance_number]++;
-        $this->cur_fetch_row = $next_record_data[$cur_record];
+        if ( $cur_record == count( $next_record_data ) ) {
+            // this is the last available record, avoid getting a warning
+            // about undefined indexes into the next_record_data array
+            $this->cur_fetch_row = false;
+        } else {
+            $this->cur_fetch_row = $next_record_data[$cur_record];
+        }
         return ( $this->cur_fetch_row );
     }
     
@@ -291,6 +412,21 @@ extends Assert
     }
 }
 
+// used to check that all instances of the mock_db_configure class have
+// called the did_db_fail method to check that the database instance
+// created did not fail in some way.
+function mkdb_check_did_db_fail_calls() {
+    global $g_mkdb_config_did_db_fail_called;
+    for ( $idx = 0; $idx<count($g_mkdb_config_did_db_fail_called);$idx++){
+        if ( !$g_mkdb_config_did_db_fail_called[$idx] ) {
+            print "<b>WARNING</b>: Instance " . $idx . " of mock db_configure "
+                . "did not call did_db_fail method<br>\n";
+        }
+    }
+}
+
+// this replaces the normal db_sourceagency classes and ensures that
+// last mock_db_configure instance defines how the database should behave
 class db_sourceagency 
 extends mock_database 
 {
